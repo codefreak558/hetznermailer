1c38b0fbf83943ea137165ebd227dfa3d8dff193
codefreak558 <46318596+codefreak558@users.noreply.github.com>
2019-01-02 14:28:02 +0200

    Update test3.php

--- examples/test3.php
+++ examples/test3.php
@@ -2,18 +2,12 @@
 require '../PHPMailerAutoload.php';
 
 $mail = new PHPMailer;
-/**$mail->isSMTP();
-$mail->SMTPDebug = 2;
-$mail->Debugoutput = 'html';
-*/
 $mail->Host = "41.204.200.12";
 $mail->Port = 25;
 $mail->SMTPAuth = false;
-//$mail->Username = "aidan@darkblue.co.za";
-//$mail->Password = "Amt20040515!";
-$mail->setFrom('aidan@darkblue.co.za', 'First Last');
-$mail->addReplyTo('aidan@darkblue.co.za', 'First Last');
-$mail->addAddress('aidantaylor140@gmail.com', 'John Doe');
+$mail->setFrom('email@email.com', 'First Last');
+$mail->addReplyTo('email@email.com', 'First Last');
+$mail->addAddress('email@email.com', 'John Doe');
 $mail->Subject = 'PHPMailer SMTP test';
 $mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
 $mail->AltBody = 'This is a plain-text message body';
